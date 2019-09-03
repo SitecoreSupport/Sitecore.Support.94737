f0d2688b0181e0f2b1bdd959812b21e7004ed2ae
Mariia Dehtiarenko <made@sitecore.net>
2019-09-03 11:27:53 +0300

    Fixed css for dialogs.

---
+++ src/Sitecore.Support.94737/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.94737")]
+[assembly: AssemblyProduct("Sitecore.Support.94737")]
+[assembly: ComVisible(false)]
